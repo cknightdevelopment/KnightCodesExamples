95910fb5f9802c5d14a2a56751ea941b828425d3
Chris Knight <cknightdevelopment@gmail.com>
2015-09-08 09:42:54 -0700

    Added quartz with XML config project

---
+++ DotNet/QuartzWithXmlConfiguration/ExampleJob.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading;
+using Quartz;
+
+namespace QuartzWithXmlConfiguration
+{
+    class ExampleJob : IJob
+    {
+        public void Execute(IJobExecutionContext context)
+        {
+            Console.WriteLine("--> {0} - {1} - Executing the example job", DateTime.Now, Thread.CurrentThread.Name);
+        }
+    }
+}
---
+++ DotNet/QuartzWithXmlConfiguration/Program.cs
@@ -0,0 +1,13 @@
+﻿using Quartz.Impl;
+
+namespace QuartzWithXmlConfiguration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var scheduler = new StdSchedulerFactory().GetScheduler();
+            scheduler.Start();
+        }
+    }
+}
